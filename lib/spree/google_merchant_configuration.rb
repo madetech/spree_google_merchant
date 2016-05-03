7ac1c4a42230bdab5c40c879f570e644e4fd79ed
Andrew Scott <amscott@openmailbox.org>
2016-05-03 16:27:59 +0100

    Updates config

--- lib/spree/google_merchant_configuration.rb
+++ lib/spree/google_merchant_configuration.rb
@@ -1,5 +1,5 @@
 class Spree::GoogleMerchantConfiguration < Spree::Preferences::Configuration
-  preference :google_merchant_title,       :string, default: 'My Spree Store'
+  preference :google_merchant_title,       :string, default: 'On-Running'
   preference :google_merchant_description, :string, default: 'RSS feed of our products'
-  preference :production_domain,           :string, default: 'http://spreecommerce.com/'
+  preference :production_domain,           :string, default: 'http://on-running.com/'
 end
